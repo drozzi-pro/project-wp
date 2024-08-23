0f8a729abd3694beaa598c55b578e64e8c18dc62
AlexBar <nesferat@mail.ru>
2024-08-23 15:14:50 +0300

    add variables in wp-config.php

---
+++ config/variables.php
@@ -0,0 +1,14 @@
+<?php
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+use function Env\env;
+
+$dotenv = Dotenv\Dotenv::createUnsafeImmutable('../', ['.env'], false);
+$isDev = false;
+
+if (file_exists('../.env')) {
+    $dotenv->load();
+    $isDev = env('WP_ENV') === 'development';
+}
+
+
+define('IS_DEV', $isDev);
